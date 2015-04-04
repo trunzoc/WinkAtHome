1a8713cccc9cff58ad02f137875e4d3aa51fb960
Craig Trunzo <craig.trunzo@gmail.com>
2015-04-04 19:59:39 -0400

    dfags

--- WinkAtHome/WinkAtHome.Master.designer.cs
+++ WinkAtHome/WinkAtHome.Master.designer.cs
@@ -76,6 +76,15 @@ namespace WinkAtHome {
         protected global::System.Web.UI.UpdatePanel UpdatePanel1;
         
         /// <summary>
+        /// Timer1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.Timer Timer1;
+        
+        /// <summary>
         /// ucMenu control.
         /// </summary>
         /// <remarks>
