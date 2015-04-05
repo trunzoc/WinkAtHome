d8b99586c356de1a4432a3e8f722ddd7c3806a4d
Craig Trunzo <craig.trunzo@gmail.com>
2015-04-05 05:42:41 -0400

    Common was missing

---
+++ WinkAtHome/Classes/Common.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WinkAtHome
+{
+    public class Common
+    {
+        public static DateTime FromUnixTime(string unixTime)
+        {
+            Double longTime;
+            bool converted = Double.TryParse(unixTime, out longTime);
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(longTime);
+        }
+
+        public static string Encrypt(string toEncrypt)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+            string key = ConfigurationManager.AppSettings["encyrptionKey"];
+
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+            hashmd5.Clear();
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            tdes.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        public static string Decrypt(string cipherString)
+        {
+            byte[] keyArray;
+
+            byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+
+            string key = ConfigurationManager.AppSettings["encyrptionKey"];
+
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+            hashmd5.Clear();
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            tdes.Clear();
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
